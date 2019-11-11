1eb8ab4c2ca7dec728e4dd7bdfb9baa7f861a1d3
Stephen Merola <merolasteve@hotmail.com>
2019-11-11 17:39:07 -0600

    latest working project & practice exam

--- ClassProject/ServerPracticeExam/app/models/widgets.js
+++ ClassProject/ServerPracticeExam/app/models/widgets.js
@@ -3,7 +3,7 @@ var Schema = Mongoose.Schema;
 
 var widgetSchema = new Schema({
     Foo: { type: String, required: true },
-    Woo: { type: Number, default: 10 },
+    Woo: { type: Number, default: '10' },
 });
 
 module.exports = Mongoose.model('Widget', widgetSchema);
